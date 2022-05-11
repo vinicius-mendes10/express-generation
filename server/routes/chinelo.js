a2b97b15a61c63b7f6a1c6e984186d23d683048e
viniciux <vinicius_q13@hotmail.com>
2022-05-11 20:28:13 -0300

    feat:adicionando rotas chinelos

---
+++ server/routes/chinelo.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.send("Só as chinela")
+})
+
+module.exports = router
+© 2022 GitHub, Inc.
+Terms
+Privacy
